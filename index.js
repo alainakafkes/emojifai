706233eaa431e7ac17597a4861509f16a841fd0a
Alaina Kafkes <alainakafkes2017@u.northwestern.edu>
2017-02-10 20:59:46 -0600

    Adds dotenv, fixes Express app instantiation

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-var app = require('express');
+var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
+var dotenv = require('dotenv');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
+
+dotenv.load();
 
 var VERIFY_TOKEN = process.env.SLACK_VERIFY_TOKEN;
 if (!VERIFY_TOKEN) {
   console.error('No SLACK_VERIFY_TOKEN provided.');
-  process.exit(1)
+  process.exit(1);
 }
 
-var PORT = process.env.PORT || 3000
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true }));
+var PORT = process.env.PORT || 3000;
 
 app.post('/analyze', (req, res) => {
   if (req.body.token != VERIFY_TOKEN) {
@@ -29,12 +33,12 @@ app.post('/analyze', (req, res) => {
     'response_type' : 'in_channel',
     'text' : msg
   });
-}
+});
 
 app.listen(PORT, (err) => {
   if (err) {
     return console.error(err)
   }
 
-console.log('Starting server on port: ', PORT))
-}
+console.log('Starting server on port: ', PORT)
+});
